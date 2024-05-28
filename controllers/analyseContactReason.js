b113b3e8e3931634df816ec02c28147b5c08b4b9
Stian Branden <stian@branden.no>
2024-05-28 20:01:18 +0200

    Added analyse module

---
+++ controllers/analyseContactReason.js
@@ -0,0 +1,57 @@
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
+const color = require('colors/safe');
+const {AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_KEY, GPT4_MODEL_NAME} = process.env
+const Transcript = require('../models/Transcript')
+const db = require('./connectDB')
+const Progress = require('progress');
+const { logErr, logStd, logTab } = require("./logger");
+
+function shortenJSON(data){
+    return {summary: data.summary,  
+        contact_reason_level1: data.contactReason.level1, 
+        contact_reason_level2: data.contactReason.level2,
+        contact_reason_level3: data.contactReason.level3 
+    }
+}
+
+function analyse(query={hasSummary: true}, maxSampleSize=1000){
+    return new Promise( async (resolve, reject)=>{
+        try {
+            const summaries = await Transcript.find(query, {summary: 1, "contactReason": 1}).lean()
+            // console.log(shortenJSON(summaries[0]))
+            logTab({"Samples found": summaries.length, "Max samples used": maxSampleSize }, "Starting analysis")
+
+            const messages = [
+                { role: "system", content: "Always answer in English."},
+                { role: "system", content: "Based on the data, create a set of 5-10 better Contact reasons Level 1s and Level 2s"},
+                { role: "system", content: "Include how many of the samples each Contact reason would get and also how many of the sample data doesnt match a contact reason" }
+            ];
+            if (summaries.length <= maxSampleSize){
+                for ( let i = 0; i < summaries.length; i++) {
+                    messages.push({role: "user", content: JSON.stringify(summaries[i])})
+                }
+            }
+            else {
+                for ( let i = 0; i < maxSampleSize; i++){
+                    const index = Math.floor(Math.random()*summaries.length)
+                    const obj = summaries.splice(index, 1)
+                    messages.push({role: 'user', content: JSON.stringify(obj)})
+                }
+            }
+
+
+            const client = new OpenAIClient(AZURE_OPENAI_ENDPOINT, new AzureKeyCredential(AZURE_OPENAI_API_KEY));
+
+            const result = await client.getChatCompletions(GPT4_MODEL_NAME, messages);
+            logStd( result.choices[0].message.content )
+
+            resolve('ok')
+        } catch (error) {
+            // logErr(error)
+            reject(error)
+        }
+
+    })
+}
+
+module.exports = analyse

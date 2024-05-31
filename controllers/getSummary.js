86ebb37e4b60337ba2d1f0784e03d3dbad869899
Stian Branden <stian@branden.no>
2024-05-31 14:04:45 +0200

    Prompt change and logging tokens used

--- controllers/getSummary.js
+++ controllers/getSummary.js
@@ -4,7 +4,9 @@ const {AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_KEY, GPT35_MODEL_NAME} = process.
 const Transcript = require('../models/Transcript')
 const db = require('./connectDB')
 const Progress = require('progress');
-const { logErr } = require("./logger");
+const { logErr, logTab } = require("./logger");
+const { createOrUpdateTokenUsage } = require("./logTokensUsed");
+const contact_reasons = require('../training_data/contact_reasons')
 // console.log({AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_KEY})
 
 
@@ -15,6 +17,7 @@ const sleep = (ms) => {
 
 function createSummaries(){
   return new Promise( async(resolve, reject)=>{
+    const status = {success: 0, failed: 0}
     try {
       // await db.connect()
 
@@ -30,22 +33,26 @@ function createSummaries(){
           logErr(transcripts[i] + ' Failed')
           logErr(JSON.stringify(error))
         }
-
+        status.success++
         aiBar.tick()
       }
       resolve('ok')
       // await db.disconnect() 
     } catch (error) {
-      logErr(JSON.stringify(error))
+      // logErr(JSON.stringify(error))
+      status.failed++
       reject(error)
     }
-    
+    finally {
+      logTab(status, "Run completed with: ")
+    }
   })
 }
 
 function summary(_id) {
   const messages = [
-    { role: "system", content: "Always answer in English. You create summaries from text transcripts of conversations with an electronics retailer customer service. In addition, using a 3 level granularity, you categorize the contact reason of the conversation. Return the summary and contact reason as a json object with the format: {\"summary\": \"example summary\", \"contact_reason\": {\"level1\": \"example level 1\", \"level2\": \"example level 2\", \"level3\": \"example level 3\"}}" },
+    // { role: "system", content: "Always answer in English. You create summaries from text transcripts of conversations with an electronics retailer customer service. In addition, using a 3 level granularity, you categorize the contact reason of the conversation. Return the summary and contact reason as a json object with the format: {\"summary\": \"example summary\", \"contact_reason\": {\"level1\": \"example level 1\", \"level2\": \"example level 2\", \"level3\": \"example level 3\"}}" },
+    { role: "system", content: "Always answer in English. You create summaries from text transcripts of conversations with an electronics retailer customer service. In addition, categorize the contact reason of the conversation using the set list of contact reasons. Return the summary and contact reason as a json object with the format: {\"summary\": \"example summary\", \"contact_reason\": {\"level1\": \"example level 1\", \"level2\": \"example level 2\"}}. The list of contact reasons are: " + JSON.stringify(contact_reasons) },
   //   { role: "user", content: "Does Azure OpenAI support customer managed keys?" },
   //   { role: "assistant", content: "Yes, customer managed keys are supported by Azure OpenAI" },
   //   { role: "user", content: "What are Azure AI services?" },
@@ -62,38 +69,59 @@ function summary(_id) {
             //     else console.log(color.blue(text.channel + ': ') + text.text);
             //     await sleep(350)                
             // }
-            messages.push({role: "user", content: JSON.stringify(transcript)})
-            // console.log()
-            // console.log('Azure OpenAI, please create a summary of this conversation and also a log the contact reason :D');
-            // console.log()
-            const result = await client.getChatCompletions(GPT35_MODEL_NAME, messages);
-            // await sleep(200)
-            
-            // console.log(color.red('Summary: '))
-            // for (const choice of result.choices) {
-              // console.log(result.choices[0].message.content);
-              const data = JSON.parse(result.choices[0].message.content)
-              // console.log(data.summary);
+            if ( transcript.transcript.length > 0){
+
+              messages.push({role: "user", content: JSON.stringify(transcript)})
+              // console.log()
+              // console.log('Azure OpenAI, please create a summary of this conversation and also a log the contact reason :D');
+              // console.log()
+              const result = await client.getChatCompletions(GPT35_MODEL_NAME, messages);
+              // await sleep(200)
               
-              // await sleep(1000)
-              // console.log()
-              // console.log(color.red('Contact reason: '))
-              // console.log('Level 1: ' + data["contact_reason"].level1);
-              // console.log('Level 2: ' + data["contact_reason"].level2);
-              // console.log('Level 3: ' + data["contact_reason"].level3);
-              // console.log()
-              // console.log()
+              // console.log(color.red('Summary: '))
+              // for (const choice of result.choices) {
+                // console.log(result.choices[0].message.content);
+              const data = JSON.parse(result.choices[0].message.content)
+                // console.log(data.summary);
+                
+                // await sleep(1000)
+                // console.log()
+                // console.log(color.red('Contact reason: '))
+                // console.log('Level 1: ' + data["contact_reason"].level1);
+                // console.log('Level 2: ' + data["contact_reason"].level2);
+                // console.log('Level 3: ' + data["contact_reason"].level3);
+                // console.log()
+                // console.log()
+                await createOrUpdateTokenUsage(result)
+                await Transcript.findByIdAndUpdate(_id, {
+                  summary: data.summary,
+                  contactReason: data["contact_reason"],
+                  hasSummary: true
+                })
+              
+    
+                resolve(result)
+            }
+            else {
+              await Transcript.findByIdAndUpdate(_id, {
+                hasError: true,
+                errorMessage: 'No transcript found',
+                hasSummary: true
+              })
+              reject({
+                message: 'No transcript found'
+              })
+            }
 
             // }
+
+        } catch (error) {
+            // console.log(error);
             await Transcript.findByIdAndUpdate(_id, {
-              summary: data.summary,
-              contactReason: data["contact_reason"],
+              hasError: true,
+              errorMessage: error.message,
               hasSummary: true
             })
-
-            resolve(result)
-        } catch (error) {
-            console.log(error);
             reject(error)
         }
     })
